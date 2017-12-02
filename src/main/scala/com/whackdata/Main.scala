f1db43f08812971cacbfb3d1766fc6c251289533
Ryan Brideau <ryan.brideau@gmail.com>
2017-12-02 14:31:11 -0500

    Tested iterating over all cells

--- src/main/scala/com/whackdata/Main.scala
+++ src/main/scala/com/whackdata/Main.scala
@@ -3,7 +3,7 @@ package com.whackdata
 import java.io.BufferedInputStream
 import java.nio.file.{Files, Paths}
 
-import com.sksamuel.scrimage.Image
+import com.sksamuel.scrimage.{Image, Pixel}
 import com.sksamuel.scrimage.filter.BlurFilter
 import org.rogach.scallop.{ScallopConf, ScallopOption}
 
@@ -17,12 +17,22 @@ object Main extends App {
   // Make a copy so that you don't modify the original
   // Use filters. Filters can be chained.
   // Use overlay
+  // Map function for going over each pixel
+  // .color returns the value at a coordinate
+  // .pix returns the pixel at a point
+
   val conf = new ParseArgs(args)
   val imagePath = Paths.get(conf.image())
 
   val imageIn = new BufferedInputStream(Files.newInputStream(imagePath))
   val imageOut = Utils.getOutputPath(imagePath)
 
-  Image.fromStream(imageIn).filter(BlurFilter).flipX.output(imageOut)
+  def printPixel(x: Int, y: Int, pixel: Pixel) = {
+    val color = pixel.toColor
+    println(s"X: $x Y: $y RGB: (${color.red}, ${color.green}, ${color.blue})")
+  }
+
+  Image.fromStream(imageIn)
+    .foreach(printPixel)
 
 }
