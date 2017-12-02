253246ad37baecf433bd3fe3e48ce4eb4cd4386d
Ryan Brideau <ryan.brideau@gmail.com>
2017-12-02 14:56:05 -0500

    Added binary classification function

--- src/main/scala/com/whackdata/Main.scala
+++ src/main/scala/com/whackdata/Main.scala
@@ -3,7 +3,7 @@ package com.whackdata
 import java.io.BufferedInputStream
 import java.nio.file.{Files, Paths}
 
-import com.sksamuel.scrimage.{Image, Pixel}
+import com.sksamuel.scrimage.{Color, Image, Pixel}
 import com.sksamuel.scrimage.filter.BlurFilter
 import org.rogach.scallop.{ScallopConf, ScallopOption}
 
@@ -27,12 +27,21 @@ object Main extends App {
   val imageIn = new BufferedInputStream(Files.newInputStream(imagePath))
   val imageOut = Utils.getOutputPath(imagePath)
 
-  def printPixel(x: Int, y: Int, pixel: Pixel) = {
+  def printPixel(x: Int, y: Int, pixel: Pixel): Pixel = {
     val color = pixel.toColor
     println(s"X: $x Y: $y RGB: (${color.red}, ${color.green}, ${color.blue})")
+    pixel
   }
 
-  Image.fromStream(imageIn)
-    .foreach(printPixel)
+  def classifyBinary(nonBorderColor: Color)(x: Int, y: Int, pixel: Pixel): Pixel = {
+    if (pixel.toColor == nonBorderColor) Pixel.apply(Color.White)
+    else Pixel.apply(Color.Black)
+  }
+  val classifyWhite = classifyBinary(Color.White)(_,_,_)
+
+  val imgCopy = Image.fromStream(imageIn).copy
+  Utils.timems {
+    imgCopy.map(classifyWhite).output(imageOut)
+  }
 
 }
--- src/main/scala/com/whackdata/Utils.scala
+++ src/main/scala/com/whackdata/Utils.scala
@@ -13,4 +13,20 @@ object Utils {
     inDirectory.resolve(outName)
   }
 
+  def timems[R](block: => R): R = {
+    val t0 = System.currentTimeMillis()
+    val result = block    // call-by-name
+    val t1 = System.currentTimeMillis()
+    println("Elapsed time: " + (t1 - t0) + "ms")
+    result
+  }
+
+  def timens[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block    // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0) + "ss")
+    result
+  }
+
 }
