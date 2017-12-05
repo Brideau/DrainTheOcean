12e26a14f21bd9d89f6167849bff8a996040451f
Ryan Brideau <ryan.brideau@gmail.com>
2017-12-05 13:36:49 -0500

    Added color ramp

--- src/main/scala/com/whackdata/GeneratePng.scala
+++ src/main/scala/com/whackdata/GeneratePng.scala
@@ -1,11 +1,14 @@
 package com.whackdata
 
-import java.nio.file.{Path, Paths}
+import java.nio.file.Paths
 
 import geotrellis.raster.io.geotiff.reader.GeoTiffReader
-import geotrellis.raster.resample.ResampleMethod
+import geotrellis.raster.render.ColorRamps
+import org.slf4j.LoggerFactory
 
 object GeneratePng {
+
+  private val logger = LoggerFactory.getLogger("PNG Logger")
 
   def run(conf: ParseArgs): Unit = {
     val baseRaster = conf.base_raster()
@@ -27,15 +30,25 @@ object GeneratePng {
     val inputPath = Paths.get(conf.base_raster())
     val outputDir = Paths.get(conf.output_path())
 
+    logger.info("Reading in base layer")
     val baseGeoTiff = GeoTiffReader.readSingleband(
       inputPath.toString,
       decompress = false,
       streaming = false
     )
 
-    val smallTile = baseGeoTiff.tile.resample(1920, 960)
+    val colourRamp = ColorRamps.ClassificationMutedTerrain
+    val (min, max) = baseGeoTiff.tile.findMinMax
+    val breaks = (max to min).by(-18).toArray
+    val colouredTile = baseGeoTiff.tile.color(colourRamp.stops(100).toColorMap(breaks))
+
+    logger.info("Resizing base layer to 1920x960")
+    val smallTile = colouredTile.resample(1920, 960)
+
+    logger.info("Rendering base layer PNG")
     val smallPng = smallTile.renderPng()
 
+    logger.info("Writing out base layer PNG")
     val outputPath = Utils.getOutputPath(inputPath, outputDir, "PNG", 0)
     smallPng.write(outputPath.toString)
   }
