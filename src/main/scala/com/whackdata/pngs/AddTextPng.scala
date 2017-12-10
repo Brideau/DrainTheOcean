0840a22725f7541d1338679121a5c9479103dde4
Ryan Brideau <ryan.brideau@gmail.com>
2017-12-10 11:04:02 -0500

    Merged animation with adding text

--- src/main/scala/com/whackdata/pngs/AddTextPng.scala
+++ src/main/scala/com/whackdata/pngs/AddTextPng.scala
@@ -1,10 +1,13 @@
 package com.whackdata.pngs
 
-import java.awt.{Color, Font}
+import java.awt.image.BufferedImage
+import java.awt.{Color, Font, Graphics2D, Image, RenderingHints}
 import java.io.File
 import java.nio.file.Paths
 import javax.imageio.ImageIO
 
+import com.madgag.gif.fmsware.AnimatedGifEncoder
+import com.sun.prism.Graphics
 import com.whackdata.Utils.{ProcessedFile, getCsv}
 import com.whackdata.{ParseArgs, Utils}
 import org.slf4j.LoggerFactory
@@ -15,12 +18,33 @@ object AddTextPng {
 
   private val logger = LoggerFactory.getLogger("Add Text Logger")
 
+  def toBufferedImage(img: Image): BufferedImage = {
+    img match {
+      case image: BufferedImage => image
+      case image =>
+        val bImage = new BufferedImage(
+          image.getWidth(null),
+          image.getHeight(null),
+          BufferedImage.TYPE_INT_ARGB
+        )
+        // Draw the image on to the buffered image
+        val bGr = bImage.createGraphics
+        bGr.drawImage(image, 0, 0, null)
+        bGr.dispose()
+        // Return the buffered image
+        bImage
+    }
+  }
+
   def run(conf: ParseArgs): Unit = {
-    val outputPath = Paths.get(conf.output_path())
-    val existingPngs = Utils.getAlreadyProcessed(outputPath, "Composed", "png")
+    val outPath = Paths.get(conf.output_path())
+    val outFile = outPath.resolve("FinalAnimation.gif")
+
+    val existingPngs = Utils.getAlreadyProcessed(outPath, "Composed", "png")
+    val existingPngsSorted = existingPngs.sortBy(-_.elev)
 
     val fileName = "TimeResults.csv"
-    val file = new File(outputPath.toString, fileName)
+    val file = new File(outPath.toString, fileName)
 
     val timeRecords = getCsv(file) match {
       case Success(result) => result
@@ -41,22 +65,35 @@ object AddTextPng {
     val times = timeRecordPrep.map(_._2)
     val cumTime = times.scanLeft(0.0)((b, a) => b + a)
 
+    logger.info("Starting encoder")
+    val encoder = new AnimatedGifEncoder
+    encoder.start(outFile.toString)
+    encoder.setRepeat(0)
+    encoder.setFrameRate(30)
 
-    def addText(layer: ProcessedFile) = {
+    def addText(layer: ProcessedFile) = Utils.timems {
       logger.info(s"Adding text for elevation ${layer.elev}")
       val text = s"${math.abs(layer.elev)}m below current sea level"
       val image = ImageIO.read(layer.path.toFile)
 
-      val graphics = image.getGraphics
+      val graphics = image.getGraphics.asInstanceOf[Graphics2D]
+      graphics.setRenderingHint(
+        RenderingHints.KEY_TEXT_ANTIALIASING,
+        RenderingHints.VALUE_TEXT_ANTIALIAS_ON
+      )
       graphics.setColor(Color.BLACK)
       graphics.setFont(new Font("Arial", Font.BOLD, 200))
       graphics.drawString(text, 20, 160)
 
-      val outName = Utils.getOutputPath(layer.path, outputPath, "WithText", layer.elev)
-      ImageIO.write(image, "png", outName.toFile)
+      // SCALE_FAST, SCALE_SMOOTH
+      val scaled = toBufferedImage(image.getScaledInstance(1920, 1087, Image.SCALE_DEFAULT))
+
+      encoder.addFrame(scaled)
     }
 
-    existingPngs.par.map(addText)
+    existingPngsSorted.foreach(addText)
+
+    encoder.finish()
 
   }
 }
